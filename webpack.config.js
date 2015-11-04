c46aa5ac1f1f6c465918a289c33cb617dccf43e9
KFlash <kflash123@gmail.com>
2015-11-04 23:18:35 +0800

    webpack.config.js fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,35 @@
 const path = require('path');
 const webpack = require('webpack');
+const pkg = require('./package.json');
+
+const plugins = [
+	new webpack.optimize.OccurenceOrderPlugin(),
+	new webpack.DefinePlugin({
+		'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+		__VERSION__: JSON.stringify(pkg.version)
+	})
+];
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        screw_ie8: true,
+        warnings: false
+      }
+    })
+  );
+}
 
 module.exports = {
   entry: {
-      main: [
-        './src'
-      ]
+      main: './src'
     },
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'trolly.js',
+    libraryTarget: 'umd',
+	library: 'trolly'
   },
 
   cache: true,
@@ -23,18 +43,21 @@ module.exports = {
     }),
     new webpack.NoErrorsPlugin()
   ],
+  resolve: {
+    extensions: ['', '.js']
+  },
   module: {
 	   preLoaders: [
         {
-          test: /\.(js|jsx)$/,
+          test: /\.js$/,
           exclude: /node_modules/,
           loader: 'eslint-loader'
         }
       ],
     loaders: [
       {
-        test: /\.jsx?$/,
-        loaders: ['babel'],
+        test: /\.js$/,
+        loaders: ['babel-loader'],
         include: path.join(__dirname, 'src'),
         exclude: /(node_modules)/
       }
