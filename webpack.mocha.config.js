fc2392d04412cd71524587d135ad48c607108050
KFlash <kflash123@gmail.com>
2015-11-06 08:34:20 +0800

    typo fixes

--- webpack.mocha.config.js
+++ webpack.mocha.config.js
@@ -1,18 +1,19 @@
 const path = require('path');
 const webpack = require('webpack');
+const testDirectory = path.join(__dirname, '__tests__');
 
 module.exports = {
-	entry: 'mocha!' + path.join(__dirname, '__tests__'); + '/bootstrap.js',
+	entry: 'mocha!' + testDirectory + '/bootstrap.js',
 	cache: true,
 	debug: true,
 	devtool: 'source-map',
 	output: {
-		path: testDir,
+		path:  testDirectory,
 		filename: 'bundle.js',
 		publicPath: 'http://localhost:8080/'
 	},
 	devServer: {
-		contentBase: path.join(__dirname, '__tests__');,
+		contentBase: testDirectory,
 		noInfo: true,
 		hot: true,
 		inline: true
@@ -21,7 +22,7 @@ module.exports = {
 		loaders: [
 			{
 				test: /\.js$/,
-				include: [path.join(__dirname, 'src'), path.join(__dirname, '__tests__')],
+				include: [path.join(__dirname, 'src'), testDirectory],
 				exclude: ['node_modules'],
 				loader: 'babel-loader'
 			}
