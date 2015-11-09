6d81a97a7f1ba6b2801f9c71595d2c550c44e965
KFlash <kflash123@gmail.com>
2015-11-09 17:38:08 +0800

    version bumb

--- __config__/webpack/webpack.server.config.js
+++ __config__/webpack/webpack.server.config.js
@@ -1,19 +1,44 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
 
 require('babel-core/register');
 
-var config = require('./webpack.config');
+const webpackConfig = require('./webpack.config');
 
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  headers: { 'Access-Control-Allow-Origin': '*' },
-  historyApiFallback: true
+// First we fire up Webpack an pass in the configuration we
+// created
+const bundleStart = null;
+const compiler = webpack(webpackConfig);
+
+// We give notice in the terminal when it starts bundling and
+// set the time it started
+compiler.plugin('compile', function() {
+    console.log('Bundling...');
+    bundleStart = Date.now();
+});
+
+// We also give notice when it is done compiling, including the
+  // time it took. Nice to have
+  compiler.plugin('done', function() {
+    console.log('Bundled in ' + (Date.now() - bundleStart) + 'ms. The server are now running...');
+  });
+
+const bundler = new WebpackDevServer(compiler, {
+    publicPath: webpackConfig.output.publicPath,
+    // Configure hot replacement
+    hot: true,
+    // The rest is terminal configurations
+    quiet: false,
+    noInfo: true,
+    stats: {
+        colors: true
+    }
+    // We fire up the development server and give notice in the terminal
+    // that we are starting the initial bundle
 }).listen(8080, 'localhost', function(error) {
-  if (error) {
-    console.log(error);
-  }
 
-  console.log('Listening at localhost:8080');
+    if (error) {
+        console.log(error);
+    }
+    console.log('Bundling project, please wait...');
 });
