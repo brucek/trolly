8367ecf92b5a28cada08d146a2baf1d4555093d9
KFlash <kflash123@gmail.com>
2015-11-16 01:29:43 +0800

    version bump

--- dist/trolly.js
+++ dist/trolly.js
@@ -1,4 +1,4 @@
-/*! trolly, version 0.5.5 */
+/*! trolly, version 0.5.7 */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
