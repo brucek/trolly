42a3154d54162a373fa3779cba6570006e99b722
KFlash <kflash123@gmail.com>
2015-11-19 07:43:59 +0800

    simplified the core settings

--- config/webpack/webpack.hot.assets.config.js
+++ config/webpack/webpack.hot.assets.config.js
@@ -1,42 +1,25 @@
+/* eslint-disable no-var, strict */
 require('babel-core/register');
 
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
-const config = require('../config');
 const environment = require('../environment');
+const config = require('./webpack.development.config');
 
-new WebpackDevServer(webpack({
-    entry: [
+config.entry = [
 
-        // For hot style updates
-        'webpack/hot/only-dev-server',
-        // The script refreshing the browser on none hot updates
-        'webpack-dev-server/client?http://localhost:8080'
-    ],
-    output: {
-        // We need to give Webpack a path. It does not actually need it,
-        // because files are kept in memory in webpack-dev-server, but an
-        // error will occur if nothing is specified. We use the buildPath
-        // as that points to where the files will eventually be bundled
-        // in production		
-        path: config.distDir,
-        filename: config.dev
-    },
-    module: {
-        loaders: [{
-            test: /\.js?$/,
-            exclude: [/node_modules/],
-            loader: 'babel-loader'
-        }]
-    },
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    },
-    plugins: [
-        // Used for hot-reload
-        new webpack.HotModuleReplacementPlugin()
-    ]
-}), {
+    // For hot style updates
+    'webpack/hot/only-dev-server',
+    // The script refreshing the browser on none hot updates
+    'webpack-dev-server/client?http://localhost:8080'
+];
+
+config.plugins = [
+    // Used for hot-reload
+    new webpack.HotModuleReplacementPlugin()
+];
+
+new WebpackDevServer(webpack(config), {
     publicPath: 'http://localhost:8080/assets/',
     // Configure hot replacement
     hot: true,
@@ -57,5 +40,5 @@ new WebpackDevServer(webpack({
     if (err) {
         console.log(err);
     }
-    console.log('The asset server is running at localhost: 8080');
+    console.log('The asset server is listening  at localhost: 8080');
 });
