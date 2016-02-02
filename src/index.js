0e43c038c4e5b0b547480612aa40aff32c414122
Florian BLOUET <florian.blouet@gmail.com>
2016-02-02 11:19:44 +0100

    fix tab space according to .editorconfig,  fix test execution over eslint

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import fooBar from './fooBar';
 /**
-* Migrating from Babel >= 5.x to Babel >= 6.x, will most likely break your code
-* because Babel now have killed the CommonJS default export behaviour.
-*
-As a workaround, replace export default { … } with module.exports = { … }
-*/
+ * Migrating from Babel >= 5.x to Babel >= 6.x, will most likely break your code
+ * because Babel now have killed the CommonJS default export behaviour.
+ *
+ As a workaround, replace export default { … } with module.exports = { … }
+ */
 
 const trolly = {
-  fooBar,
-  example() {
-    return 'hello';
-  }
+    fooBar,
+    example() {
+        return 'hello';
+    }
 };
 
 module.exports = trolly;
